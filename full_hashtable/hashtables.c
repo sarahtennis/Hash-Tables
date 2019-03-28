44685a168886d0279e2bec90562d46e96c002d45
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-28 15:01:39 -0400

    full hash table - create_hash_table() and hash_table_insert()

--- full_hashtable/hashtables.c
+++ full_hashtable/hashtables.c
@@ -9,7 +9,8 @@
   More specifically, the `next` field is a pointer pointing to the the 
   next `LinkedPair` in the list of `LinkedPair` nodes. 
  */
-typedef struct LinkedPair {
+typedef struct LinkedPair
+{
   char *key;
   char *value;
   struct LinkedPair *next;
@@ -18,7 +19,8 @@ typedef struct LinkedPair {
 /*
   Hash table with linked pairs.
  */
-typedef struct HashTable {
+typedef struct HashTable
+{
   int capacity;
   LinkedPair **storage;
 } HashTable;
@@ -41,7 +43,8 @@ LinkedPair *create_pair(char *key, char *value)
  */
 void destroy_pair(LinkedPair *pair)
 {
-  if (pair != NULL) {
+  if (pair != NULL)
+  {
     free(pair->key);
     free(pair->value);
     free(pair);
@@ -57,14 +60,18 @@ unsigned int hash(char *str, int max)
 {
   unsigned long hash = 5381;
   int c;
-  unsigned char * u_str = (unsigned char *)str;
+  unsigned char *u_str = (unsigned char *)str;
 
-  while ((c = *u_str++)) {
+  while ((c = *u_str++))
+  {
     hash = ((hash << 5) + hash) + c;
   }
 
   return hash % max;
 }
+
+// int capacity;
+// LinkedPair **storage; --> char *key; char *value; struct LinkedPair *next;
 
 /*
   Fill this in.
@@ -73,7 +80,14 @@ unsigned int hash(char *str, int max)
  */
 HashTable *create_hash_table(int capacity)
 {
-  HashTable *ht;
+  // initialize instance of HashTable
+  HashTable *ht = malloc(sizeof(HashTable));
+
+  // set capacity to input value (allocated space part of HashTable instance)
+  ht->capacity = capacity;
+
+  // allocate space for pair storage (space for pairs created elsewhere)
+  ht->storage = (LinkedPair **)calloc(capacity, sizeof(LinkedPair *));
 
   return ht;
 }
@@ -89,10 +103,64 @@ HashTable *create_hash_table(int capacity)
  */
 void hash_table_insert(HashTable *ht, char *key, char *value)
 {
+  // hash key
+  unsigned int hashIndex = hash(key, ht->capacity);
 
-}
+  // create new pair
+  LinkedPair *newPair = create_pair(key, value);
 
-/*
+  // if index is empty
+  if (!ht->storage[hashIndex])
+  {
+    ht->storage[hashIndex] = newPair;
+  }
+  else
+  {
+    // if not empty, either replace or add to linked list
+    // insert B: A->B->C
+    LinkedPair *previous = NULL;
+    LinkedPair *current = ht->storage[hashIndex];
+
+    do
+    {
+      // key of current pair is same as parameter key
+      if (strcmp(current->key, key) == 0)
+      {
+        // no previous, first/only in linked list
+        if (!previous)
+        {
+          ht->storage[hashIndex] = newPair;
+          newPair->next = current->next;
+          destroy_pair(current);
+        }
+        else
+        {
+          // not first/only element
+          ht->storage[hashIndex] = newPair;
+          previous->next = newPair;
+          newPair->next = current->next;
+          destroy_pair(current);
+        }
+        break;
+      }
+      else
+      {
+        if (current->next)
+        {
+          previous = current;
+          current = current->next;
+        }
+        else
+        {
+          current->next = newPair;
+          break;
+        }
+      }
+
+    } while (current);
+  }
+
+  /*
   Fill this in.
 
   Should search the entire list of LinkedPairs for existing
@@ -100,12 +168,11 @@ void hash_table_insert(HashTable *ht, char *key, char *value)
 
   Don't forget to free any malloc'ed memory!
  */
-void hash_table_remove(HashTable *ht, char *key)
-{
+  void hash_table_remove(HashTable * ht, char *key)
+  {
+  }
 
-}
-
-/*
+  /*
   Fill this in.
 
   Should search the entire list of LinkedPairs for existing
@@ -113,22 +180,21 @@ void hash_table_remove(HashTable *ht, char *key)
 
   Return NULL if the key is not found.
  */
-char *hash_table_retrieve(HashTable *ht, char *key)
-{
-  return NULL;
-}
+  char *hash_table_retrieve(HashTable * ht, char *key)
+  {
+    return NULL;
+  }
 
-/*
+  /*
   Fill this in.
 
   Don't forget to free any malloc'ed memory!
  */
-void destroy_hash_table(HashTable *ht)
-{
+  void destroy_hash_table(HashTable * ht)
+  {
+  }
 
-}
-
-/*
+  /*
   Fill this in.
 
   Should create a new hash table with double the capacity
@@ -136,35 +202,34 @@ void destroy_hash_table(HashTable *ht)
 
   Don't forget to free any malloc'ed memory!
  */
-HashTable *hash_table_resize(HashTable *ht)
-{
-  HashTable *new_ht;
+  HashTable *hash_table_resize(HashTable * ht)
+  {
+    HashTable *new_ht;
 
-  return new_ht;
-}
-
+    return new_ht;
+  }
 
 #ifndef TESTING
-int main(void)
-{
-  struct HashTable *ht = create_hash_table(2);
+  int main(void)
+  {
+    struct HashTable *ht = create_hash_table(2);
 
-  hash_table_insert(ht, "line_1", "Tiny hash table\n");
-  hash_table_insert(ht, "line_2", "Filled beyond capacity\n");
-  hash_table_insert(ht, "line_3", "Linked list saves the day!\n");
+    hash_table_insert(ht, "line_1", "Tiny hash table\n");
+    hash_table_insert(ht, "line_2", "Filled beyond capacity\n");
+    hash_table_insert(ht, "line_3", "Linked list saves the day!\n");
 
-  printf("%s", hash_table_retrieve(ht, "line_1"));
-  printf("%s", hash_table_retrieve(ht, "line_2"));
-  printf("%s", hash_table_retrieve(ht, "line_3"));
+    printf("%s", hash_table_retrieve(ht, "line_1"));
+    printf("%s", hash_table_retrieve(ht, "line_2"));
+    printf("%s", hash_table_retrieve(ht, "line_3"));
 
-  int old_capacity = ht->capacity;
-  ht = hash_table_resize(ht);
-  int new_capacity = ht->capacity;
+    int old_capacity = ht->capacity;
+    ht = hash_table_resize(ht);
+    int new_capacity = ht->capacity;
 
-  printf("\nResizing hash table from %d to %d.\n", old_capacity, new_capacity);
+    printf("\nResizing hash table from %d to %d.\n", old_capacity, new_capacity);
 
-  destroy_hash_table(ht);
+    destroy_hash_table(ht);
 
-  return 0;
-}
+    return 0;
+  }
 #endif
